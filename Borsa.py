9c4a7f4dc3f940ffb12b109c93aeb344245ae642
kengelkazim2323-source <kengelkazim2323@gmail.com>
2026-03-10 02:46:37 +0300

    Update Borsa.py

--- Borsa.py
+++ Borsa.py
@@ -287,14 +287,7 @@ if st.session_state.portfoy:
             for _, r in df_div.sort_values(by="NetTemettu", ascending=False).iterrows():
                 if r['Temettu'] > 0:
                     birim = "₺" if r['Piyasa'] == "Türk Borsası" else "$"                                                     
-                   
-                    table_html += "<tr>"
-                table_html += f"<td><b>{r['VARLIK']}</b></td>"
-                table_html += f"<td>{r['ADET']}</td>"
-                table_html += f"<td>{r['HİSSE BAŞI']}</td>"
-                table_html += f"<td>{tr_format(r['YILLIK NET GELİR'])}</td>"
-                   
-                cc1, cc2, cc3, cc4 = st.columns([2, 1, 1, 1.5])
+                            cc1, cc2, cc3, cc4 = st.columns([2, 1, 1, 1.5])
                     cc1.write(f"**{r['Hisse']}**")
                     cc2.write(f"{r['Adet']}")
                     cc3.write(f"{tr_format(r['Temettu'])} {birim}")
