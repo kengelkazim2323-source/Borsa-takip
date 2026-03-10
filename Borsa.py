afa94017ab847bafb285e61d11f0899da1672d1f
kengelkazim2323-source <kengelkazim2323@gmail.com>
2026-03-10 10:58:37 +0300

    Update Borsa.py

--- Borsa.py
+++ Borsa.py
@@ -267,7 +267,7 @@ with st. expander ("ADET GUNCELLE") :
 for idx, r in
 df.iterrows () :
 ucl, uc2, uc3 =
-st. columns ( [2, 2, 21)
+st. columns ( [2, 2, 21])
 uc1. markdown (f"<div
 style='margin-top:8px;'×b>{r[ 'Hisse' 1}</b>
 (Mevcut: {r['Adet' ]})</div>"
