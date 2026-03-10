f23684d8d1083b5b3571b219405c3152da566e69
kengelkazim2323-source <kengelkazim2323@gmail.com>
2026-03-10 14:53:41 +0300

    Update Borsa.py

--- Borsa.py
+++ Borsa.py
@@ -236,6 +236,29 @@ if st.session_state.portfoy:
         
             # --- YATAY SATIR VE SÜTUNLU TABLO ---
 
+            table html = """
+<style>
+.kral-table { 
+width: 100%;
+table-layout: fixed; /* Sütunları
+eşit genişliğe zorlar */
+text-align: center;
+* Her şeyi
+ortalar, simetri sağlar */
+{
+.kral-table th, .kral-table ta { border-bottom: 1px solid #444; /*
+Satırları ayırmak için ince çizgi */
+padding: 8px;
+}
+</ style>
+
+
+
+
+
+
+
+            
             table_html = "<table class='kral-table'><thead><tr>"
             table_html += "<th>HİSSE</th><th>SİNYAL</th><th>ADET</th><th>MALİYET(₺)</th><th>GÜNCEL(₺)</th><th>K/Z(₺)</th><th>TOPLAM(₺)</th>"
             table_html += "</tr></thead><tbody>"
